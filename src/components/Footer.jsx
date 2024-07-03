b4eed044f8748cfee30b9426ee1eb2c8047aeded
Victor dS <victordespirlet@gmail.com>
2024-07-03 18:45:56 +0200

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-2 border-t-2 border-gray-200">
+        <p className="text-sm text-center uppercase">Boxy Generator - All Rights Reserved ©</p>
+    </footer>
+  )
+}
---
+++ src/layouts/Visualization/Modal/ModalResult.jsx
@@ -0,0 +1,62 @@
+import getBoxShadowValue from "../../../utils/getBoxShadowValue"
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
+
+export default function ModalResult({ closeModal }) {
+  const shadowValues = useSelector((state) => state.shadows);
+
+
+  // scroll deactivate with modal
+  useEffect(() => {
+    document.body.style.overflowY = "hidden"
+
+    return () => document.body.style.overflowY = "auto"
+  }, [])
+  // scroll deactivate with modal
+
+  let runningAnimation = false
+  function copyPaste(e){
+    if(!runningAnimation){
+      runningAnimation = true 
+      e.target.textContent = "Copied !"
+
+      setTimeout(() => {
+        e.target.textContent = "Copy"
+        runningAnimation = false
+      }, 1250)
+    }
+
+    navigator.clipboard.writeText(`box-shadow: ${getBoxShadowValue(shadowValues)}`)
+  }
+
+
+  return (
+    <div
+      onClick={closeModal}
+      className="fixed z-10 inset-0 flex items-center justify-center bg-gray-600/75"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-[400px] rounded p-7 bg-gray-50 mb-[10vh]"
+      >
+        <div className="flex items mb-5">
+          <p className="font-semibold mr-5">Here is your code 🎉</p>
+          <button onClick={copyPaste} className="ml-auto mr-2 bg-blue-600 hover:bg-blue-700 text-white text-sm rounded  py-1 px-3">
+            Copy
+          </button>
+          <button
+            onClick={closeModal}
+            className="bg-red-600 hover:bg-red-700 text-white text-sm rounded  py-1 px-3"
+          >
+            Close
+          </button>
+        </div>
+        <p className="rounded bg-gray-100 p-5">
+            <span className="font-semibold">box-shadow: </span>
+            <span>{getBoxShadowValue(shadowValues)}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
